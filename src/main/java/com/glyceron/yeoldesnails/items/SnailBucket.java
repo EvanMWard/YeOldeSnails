120292b172b99cb46b68adc14c8044334622be84
EvanMWard <evanward@protonmail.com>
2020-12-08 15:24:21 -0500

    Resolving merge conflicts

--- src/main/java/com/glyceron/yeoldesnails/items/SnailBucket.java
+++ src/main/java/com/glyceron/yeoldesnails/items/SnailBucket.java
@@ -1,15 +1,34 @@
 package com.glyceron.yeoldesnails.items;
 
 import com.glyceron.yeoldesnails.YeOldeSnails;
+import com.glyceron.yeoldesnails.entities.SnailEntity;
+import com.glyceron.yeoldesnails.init.ModEntityTypes;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.item.BucketItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
-public class SnailBucket extends Item {
+import java.util.function.Supplier;
+
+
+public class SnailBucket extends Item{
+
     public SnailBucket() {
         super(new Item.Properties()
         .group(YeOldeSnails.TAB)
-        .containerItem(Items.BUCKET)
         .maxStackSize(1));
     }
 
+    public void onLiquidPlaced(World worldIn, ItemStack stack, BlockPos pos){
+        if(worldIn instanceof ServerWorld){
+            Entity entity = ModEntityTypes.SNAIL.get().spawn((ServerWorld) worldIn, stack, (PlayerEntity) null, pos, SpawnReason.BUCKET, true, false);
+        }
+    }
 }
