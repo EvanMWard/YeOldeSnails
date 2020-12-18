f6955d28db7c0012a6c93f12d6d5d22ef1fced87
Navinate <treycluff@gmail.com>
2020-12-17 23:37:22 -0500

    added sound effect to placing a snail from a bucket, added tempt items for snail, added breed goal (currently broken), added new compost gather goal (also not working)

--- src/main/java/com/glyceron/yeoldesnails/entities/SnailEntity.java
+++ src/main/java/com/glyceron/yeoldesnails/entities/SnailEntity.java
@@ -1,43 +1,42 @@
 package com.glyceron.yeoldesnails.entities;
 
+import com.glyceron.yeoldesnails.init.ModEntityTypes;
 import com.glyceron.yeoldesnails.init.ModItems;
-import com.glyceron.yeoldesnails.items.SnailBucket;
 import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.ComposterBlock;
 import net.minecraft.entity.AgeableEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MobEntity;
-import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
-import net.minecraft.entity.ai.goal.LookAtGoal;
-import net.minecraft.entity.ai.goal.LookRandomlyGoal;
-import net.minecraft.entity.ai.goal.PanicGoal;
-import net.minecraft.entity.ai.goal.WaterAvoidingRandomWalkingGoal;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.server.ServerWorld;
 
 import javax.annotation.Nullable;
-import java.util.Random;
 
 public class SnailEntity extends AnimalEntity {
+
+    public static final Ingredient TEMPTATION_ITEMS = Ingredient.fromItems(Items.DANDELION, Items.CORNFLOWER, Items.BROWN_MUSHROOM, Items.RED_MUSHROOM);
 
     public SnailEntity(EntityType<? extends AnimalEntity> type, World worldIn) {
         super(type, worldIn);
     }
 
-    //func_233666_p -> registerAttributes()
+    //func_233666_p_ ---> registerAttributes()
     public static AttributeModifierMap.MutableAttribute setCustomAttributes(){
         return MobEntity.func_233666_p_()
                 .createMutableAttribute(Attributes.MAX_HEALTH, 4.0D)
@@ -47,20 +46,31 @@ public class SnailEntity extends AnimalEntity {
     @Override
     protected void registerGoals(){
         super.registerGoals();
-        this.goalSelector.addGoal(0, new PanicGoal(this, .5D));
-        this.goalSelector.addGoal(1, new LookAtGoal(this, PlayerEntity.class, 6.0F));
-        this.goalSelector.addGoal(2, new LookRandomlyGoal(this));
-        this.goalSelector.addGoal(3, new WaterAvoidingRandomWalkingGoal(this, 0.1F));
+        //SPEED VARIABLES ARE FOR TESTING
+        this.goalSelector.addGoal(0, new PanicGoal(this, 1.25D));
+        this.goalSelector.addGoal(1, new BreedGoal(this, 1.0D));
+        this.goalSelector.addGoal(2, new TemptGoal(this, 10.D, TEMPTATION_ITEMS, false));
+        this.goalSelector.addGoal(3, new LookAtGoal(this, PlayerEntity.class, 6.0F));
+        //testing goal
+        this.goalSelector.addGoal(4, new GatherNearComposterGoal(this, 1.0D));
+        this.goalSelector.addGoal(5, new LookRandomlyGoal(this));
+        this.goalSelector.addGoal(6, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
+    }
+
+    @Override
+    protected int getExperiencePoints(PlayerEntity player) {
+        return 1 + this.world.rand.nextInt(2);
     }
 
     @Nullable
     @Override
+    // create child method
     public AgeableEntity func_241840_a(ServerWorld p_241840_1_, AgeableEntity p_241840_2_) {
-        return null;
+        return ModEntityTypes.SNAIL.get().create(this.world);
     }
 
-
     // Method copied and modified from Alexthe666's AlexMobs mod
+    // Method for snails being picked up by buckets
     public ActionResultType func_230254_b_(PlayerEntity p_230254_1_, Hand p_230254_2_) {
         ItemStack itemstack = p_230254_1_.getHeldItem(p_230254_2_);
         if (itemstack.getItem() == Items.BUCKET && this.isAlive()) {
@@ -81,6 +91,37 @@ public class SnailEntity extends AnimalEntity {
             return ActionResultType.func_233537_a_(this.world.isRemote);
         } else {
             return super.func_230254_b_(p_230254_1_, p_230254_2_);
+        }
+    }
+
+    public class GatherNearComposterGoal extends MoveToBlockGoal {
+        private final SnailEntity snail;
+
+        private GatherNearComposterGoal(SnailEntity snail, double speedIn) {
+            super(snail, speedIn, 24);
+            this.snail = snail;
+            this.field_203112_e = -1;
+        }
+
+        public boolean shouldContinueExecuting() {
+            return this.timeoutCounter <= 600 && this.shouldMoveTo(this.snail.world, this.destinationBlock);
+        }
+
+        public boolean shouldExecute() {
+            return super.shouldExecute();
+        }
+
+        public boolean shouldMove() {
+            return this.timeoutCounter % 160 == 0;
+        }
+
+        protected boolean shouldMoveTo(IWorldReader worldIn, BlockPos pos) {
+            BlockState blockstate = worldIn.getBlockState(pos);
+            if(blockstate.isIn(Blocks.COMPOSTER)) {
+                return blockstate.get(ComposterBlock.LEVEL) >= 1;
+            } else {
+                return false;
+            }
         }
     }
 
--- src/main/java/com/glyceron/yeoldesnails/items/SnailBucket.java
+++ src/main/java/com/glyceron/yeoldesnails/items/SnailBucket.java
@@ -2,12 +2,14 @@ package com.glyceron.yeoldesnails.items;
 
 import com.glyceron.yeoldesnails.YeOldeSnails;
 import com.glyceron.yeoldesnails.init.ModEntityTypes;
+import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceContext;
@@ -29,25 +31,29 @@ public class SnailBucket extends Item{
     }
 
     @Override
+    @MethodsReturnNonnullByDefault
     public ActionResultType onItemUse(ItemUseContext context) {
+        ActionResultType result = ActionResultType.PASS;
 
-        World worldIn =  context.getWorld();
+        World worldIn = context.getWorld();
         PlayerEntity playerIn = context.getPlayer();
         ItemStack itemstack = context.getItem();
 
-        // gets BlockPos and face to spawn in the snail
-        RayTraceResult raytraceresult = rayTrace(worldIn, playerIn, RayTraceContext.FluidMode.ANY);
-        BlockRayTraceResult blockraytraceresult = (BlockRayTraceResult)raytraceresult;
-        BlockPos blockpos = blockraytraceresult.getPos();
-        Direction direction = blockraytraceresult.getFace();
-        BlockPos pos = blockpos.offset(direction);
+        if (playerIn != null) {
+            BlockRayTraceResult blockraytraceresult = rayTrace(worldIn, playerIn, RayTraceContext.FluidMode.ANY);
+            BlockPos blockpos = blockraytraceresult.getPos();
+            Direction direction = blockraytraceresult.getFace();
+            BlockPos pos = blockpos.offset(direction);
 
-        if(worldIn instanceof ServerWorld) {
-            Entity entity = ModEntityTypes.SNAIL.get().spawn((ServerWorld) worldIn, itemstack, (PlayerEntity) null, pos, SpawnReason.BUCKET, true, false);
-            itemstack.shrink(1);
-            playerIn.addItemStackToInventory(new ItemStack(Items.BUCKET));
+            if (worldIn instanceof ServerWorld) {
+                Entity entity = ModEntityTypes.SNAIL.get().spawn((ServerWorld) worldIn, itemstack, (PlayerEntity) null, pos, SpawnReason.BUCKET, true, false);
+                playerIn.playSound(SoundEvents.ITEM_BUCKET_EMPTY_FISH, 1.0F, 1.0F);
+                itemstack.shrink(1);
+                playerIn.addItemStackToInventory(new ItemStack(Items.BUCKET));
+            }
+            result = ActionResultType.SUCCESS;
         }
-        return ActionResultType.SUCCESS;
+        return result;
     }
 }
 
